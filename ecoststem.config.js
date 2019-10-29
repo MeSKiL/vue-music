2f7bcf202c053afccd38cfb8f8df8183cb587e2c
MeSKiL <403797970@qq.com>
2019-10-29 09:42:45 +0800

    完成业务功能开发

---
+++ ecoststem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  app: [
+    {
+      name: 'vue-music',
+      script: './prod.server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
